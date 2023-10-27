2810c7033e0a7e921e6272a5c4db08b200177648
Chi-Sing-Thao <89988545+Chi-Sing-Thao@users.noreply.github.com>
2023-10-27 11:36:51 -0500

    added map for godot to window inputs
     (still need to add mouse wheel)

--- addons/global_input_plugin/autoload/global_input/GlobalInput.cs
+++ addons/global_input_plugin/autoload/global_input/GlobalInput.cs
@@ -46,6 +46,7 @@ public partial class GlobalInput : Node
             throw new NotImplementedException();
         }
     }
+	
 	// Mouse
 	/// <summary>
 	/// Sets the mouse cursor to the give x and y cordinat without imitating mouse movement.
@@ -77,7 +78,7 @@ public partial class GlobalInput : Node
 	/// <param name="dwExtraInfo"></param>
     [DllImport("user32.dll")]
     private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
-	
+
 	[Flags]
 	public enum MouseButton{
 		Left = 0,
@@ -167,8 +168,6 @@ public partial class GlobalInput : Node
 	[DllImport("user32.dll", SetLastError = true)]
 	static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
 
-	[DllImport("user32.dll")]static extern short VkKeyScan(char ch);
-
 	[Flags]
 	public enum KeyEventF{
 		KeyDown = 0x0000,
@@ -218,8 +217,125 @@ public partial class GlobalInput : Node
 	}
 
     // Godot Section
-	/*
-	{
+	Dictionary<string, int> GodotKeyToWindowKey = new() {
+		// none
+		{Key.None.ToString(), 0x00},
+		// special
+		{Key.Backspace.ToString(), 0x08},
+		{Key.Tab.ToString(), 0x09},
+		{Key.Clear.ToString(), 0x0C},
+		{Key.Enter.ToString(), 0x0D},
+		// modifiers
+		{Key.Shift.ToString(), 0x10},
+		{Key.Ctrl.ToString(), 0x11},
+		{Key.Alt.ToString(), 0x12},
+		{Key.Pause.ToString(), 0x13},
+		{Key.Capslock.ToString(), 0x14},
+		{Key.Escape.ToString(), 0x1B},
+		// space
+		{Key.Space.ToString(), 0x20},
+		// island buttons
+		{Key.Pageup.ToString(), 0x21},
+		{Key.Pagedown.ToString(), 0x22},
+		{Key.End.ToString(), 0x23},
+		{Key.Home.ToString(), 0x24},
+		// arrow keys
+		{Key.Left.ToString(), 0x25},
+		{Key.Up.ToString(), 0x26},
+		{Key.Right.ToString(), 0x27},
+		{Key.Down.ToString(), 0x28},
+		// 
+		{Key.Print.ToString(), 0x2C},
+		{Key.Insert.ToString(), 0x2D},
+		{Key.Delete.ToString(), 0x2E},
+		{Key.Help.ToString(), 0x2F},
+		// top numbers
+		{Key.Key0.ToString(), 0x30},
+		{Key.Key1.ToString(), 0x31},
+		{Key.Key2.ToString(), 0x32},
+		{Key.Key3.ToString(), 0x33},
+		{Key.Key4.ToString(), 0x34},
+		{Key.Key5.ToString(), 0x35},
+		{Key.Key6.ToString(), 0x36},
+		{Key.Key7.ToString(), 0x37},
+		{Key.Key8.ToString(), 0x38},
+		{Key.Key9.ToString(), 0x39},
+		// letters
+		{Key.A.ToString(), 0x41},
+		{Key.B.ToString(), 0x42},
+		{Key.C.ToString(), 0x43},
+		{Key.D.ToString(), 0x44},
+		{Key.E.ToString(), 0x45},
+		{Key.F.ToString(), 0x46},
+		{Key.G.ToString(), 0x47},
+		{Key.H.ToString(), 0x48},
+		{Key.I.ToString(), 0x49},
+		{Key.J.ToString(), 0x4A},
+		{Key.K.ToString(), 0x4B},
+		{Key.L.ToString(), 0x4C},
+		{Key.M.ToString(), 0x4D},
+		{Key.N.ToString(), 0x4E},
+		{Key.O.ToString(), 0x4F},
+		{Key.P.ToString(), 0x50},
+		{Key.Q.ToString(), 0x51},
+		{Key.R.ToString(), 0x52},
+		{Key.S.ToString(), 0x53},
+		{Key.T.ToString(), 0x54},
+		{Key.U.ToString(), 0x55},
+		{Key.V.ToString(), 0x56},
+		{Key.W.ToString(), 0x57},
+		{Key.X.ToString(), 0x58},
+		{Key.Y.ToString(), 0x59},
+		{Key.Z.ToString(), 0x5A},
+		// window key
+		{"Windows", 0x5B},
+		// numberpad numbers
+		{Key.Kp0.ToString(), 0x60},
+		{Key.Kp1.ToString(), 0x61},
+		{Key.Kp2.ToString(), 0x62},
+		{Key.Kp3.ToString(), 0x63},
+		{Key.Kp4.ToString(), 0x64},
+		{Key.Kp5.ToString(), 0x65},
+		{Key.Kp6.ToString(), 0x66},
+		{Key.Kp7.ToString(), 0x67},
+		{Key.Kp8.ToString(), 0x68},
+		{Key.Kp9.ToString(), 0x69},
+		// nubmerpad operators
+		{Key.KpMultiply.ToString(), 0x6A},
+		{Key.KpAdd.ToString(), 0x6B},
+		{Key.KpSubtract.ToString(), 0x6C},
+		{Key.KpPeriod.ToString(), 0x6D},
+		{Key.KpDivide.ToString(), 0x6E},
+		// f-keys
+		{Key.F1.ToString(), 0x70},
+		{Key.F2.ToString(), 0x71},
+		{Key.F3.ToString(), 0x72},
+		{Key.F4.ToString(), 0x73},
+		{Key.F5.ToString(), 0x74},
+		{Key.F6.ToString(), 0x75},
+		{Key.F7.ToString(), 0x76},
+		{Key.F8.ToString(), 0x77},
+		{Key.F9.ToString(), 0x78},
+		{Key.F10.ToString(), 0x79},
+		{Key.F11.ToString(), 0x7A},
+		{Key.F12.ToString(), 0x7B},
+		// numlock & scroll lock
+		{Key.Numlock.ToString(), 0x90},
+		{Key.Scrolllock.ToString(), 0x91},
+		// OEM keys
+		{Key.Quoteleft.ToString(), 0xC0},
+
+	};
+	
+	Dictionary<string, int> GodotMouseToWindowMouse = new() {
+		{MouseButton.Left.ToString().ToLower(), 0x01},
+		{MouseButton.Right.ToString().ToLower(), 0x02},
+		{MouseButton.Middle.ToString().ToLower(), 0x04},
+		{MouseButton.XButton1.ToString().ToLower(), 0x05},
+		{MouseButton.XButton2.ToString().ToLower(), 0x06},
+	};
+	
+	/*{
 		<StringName action>: {
 			<int EventCode>: {
 				"pressedState", false,
@@ -230,12 +346,12 @@ public partial class GlobalInput : Node
 				...
 			},
 		}
-	}
-	*/
+	}*/
 	public Dictionary ActionDictionary = new Dictionary();
 
     public override void _Ready()
     {
+		GD.Print(GodotMouseToWindowMouse);
 		foreach(string action in InputMap.GetActions()){
 			ActionDictionary.Add(action, new Dictionary<string,Dictionary<string,bool>>());
 			foreach(InputEvent e in InputMap.ActionGetEvents(action)){
@@ -270,7 +386,7 @@ public partial class GlobalInput : Node
 	public bool IsActionJustPressed(string action){
 		foreach(InputEvent e in InputMap.ActionGetEvents(action)){ // get all inputs within action
 			if (e is InputEventMouseButton eventMouseButton){ // if input is a key
-				Dictionary EventDictionary = ((Dictionary)((Dictionary)ActionDictionary[action])[eventMouseButton.ButtonIndex.ToString()]);
+				Dictionary EventDictionary = (Dictionary)((Dictionary)ActionDictionary[action])[eventMouseButton.ButtonIndex.ToString()];
 				EventDictionary["clickedPrevState"] = EventDictionary["clickedState"];
 				EventDictionary["clickedState"] = GetKeyboardState(GetInputEventIdentifyer(eventMouseButton)) < 0;
 				bool state = (bool)EventDictionary["clickedState"];
@@ -280,7 +396,7 @@ public partial class GlobalInput : Node
 				}
 			}
 			else if (e is InputEventKey eventKey){
-				Dictionary EventDictionary = ((Dictionary)((Dictionary)ActionDictionary[action])[eventKey.AsText()]);
+				Dictionary EventDictionary = (Dictionary)((Dictionary)ActionDictionary[action])[eventKey.AsText()];
 				EventDictionary["clickedPrevState"] = EventDictionary["clickedState"];
 				EventDictionary["clickedState"] = GetKeyboardState(GetInputEventIdentifyer(eventKey)) < 0;
 				bool state = (bool)EventDictionary["clickedState"];
@@ -296,7 +412,7 @@ public partial class GlobalInput : Node
 	public bool IsActionPressed(string action){
 		foreach(InputEvent e in InputMap.ActionGetEvents(action)){ // get all inputs within action
 			if (e is InputEventMouseButton eventMouseButton){ // if input is a mouse button
-				Dictionary EventDictionary = ((Dictionary)((Dictionary)ActionDictionary[action])[eventMouseButton.ButtonIndex.ToString()]);
+				Dictionary EventDictionary = (Dictionary)((Dictionary)ActionDictionary[action])[eventMouseButton.ButtonIndex.ToString()];
 				EventDictionary["pressedPrevState"] = EventDictionary["pressedState"];
 				EventDictionary["pressedState"] = GetKeyboardState(GetInputEventIdentifyer(eventMouseButton)) < 0;
 				bool state = (bool)EventDictionary["pressedState"];
@@ -305,7 +421,7 @@ public partial class GlobalInput : Node
 				}
 			}
 			else if (e is InputEventKey eventKey){ // if input is a mouse button
-				Dictionary EventDictionary = ((Dictionary)((Dictionary)ActionDictionary[action])[eventKey.AsText()]);
+				Dictionary EventDictionary = (Dictionary)((Dictionary)ActionDictionary[action])[eventKey.AsText()];
 				EventDictionary["pressedPrevState"] = EventDictionary["pressedState"];
 				EventDictionary["pressedState"] = GetKeyboardState(GetInputEventIdentifyer(eventKey)) < 0;
 				bool state = (bool)EventDictionary["pressedState"];
@@ -320,7 +436,7 @@ public partial class GlobalInput : Node
 	public bool IsActionJustReleased(string action){
 		foreach(InputEvent e in InputMap.ActionGetEvents(action)){
 			if (e is InputEventMouseButton eventMouseButton){
-				Dictionary EventDictionary = ((Dictionary)((Dictionary)ActionDictionary[action])[eventMouseButton.ButtonIndex.ToString()]);
+				Dictionary EventDictionary = (Dictionary)((Dictionary)ActionDictionary[action])[eventMouseButton.ButtonIndex.ToString()];
 				EventDictionary["releasedPrevState"] = EventDictionary["releasedState"];
 				EventDictionary["releasedState"] = GetKeyboardState(GetInputEventIdentifyer(eventMouseButton)) < 0;
 				bool state = (bool)EventDictionary["releasedState"];
@@ -330,7 +446,7 @@ public partial class GlobalInput : Node
 				}
 			}
 			else if (e is InputEventKey eventKey){
-				Dictionary EventDictionary = ((Dictionary)((Dictionary)ActionDictionary[action])[eventKey.AsText()]);
+				Dictionary EventDictionary = (Dictionary)((Dictionary)ActionDictionary[action])[eventKey.AsText()];
 				EventDictionary["releasedPrevState"] = EventDictionary["releasedState"];
 				EventDictionary["releasedState"] = GetKeyboardState(GetInputEventIdentifyer(eventKey)) < 0;
 				bool state = (bool)EventDictionary["releasedState"];
@@ -345,17 +461,16 @@ public partial class GlobalInput : Node
 
 	public int GetInputEventIdentifyer(InputEvent e){
 		if (e is InputEventMouseButton eventMouseButton){
-			return (int)eventMouseButton.ButtonIndex;
+			GD.Print(eventMouseButton.ButtonIndex.ToString().ToLower());
+			return GodotMouseToWindowMouse[eventMouseButton.ButtonIndex.ToString().ToLower()];
 		}
 		else if (e is InputEventKey eventKey){
-			char c = (Char)(int)eventKey.PhysicalKeycode;
-			KeycodeHelper helper = new KeycodeHelper { Value = VkKeyScan(c)};
-			byte VirtualKeyCode = helper.Low;
-			byte ModifiyerState = helper.High;
-			Key GodotKeyText = eventKey.KeyLabel;
-			return (int)VirtualKeyCode;
+			string keycodeString = OS.GetKeycodeString(eventKey.PhysicalKeycode); // get the name of the key
+			// get the window keycode base on the GodotKeyToWindowKey map and return it
+			int windowKeyCode = GodotKeyToWindowKey.ContainsKey(keycodeString) ? GodotKeyToWindowKey[keycodeString] : 0;
+			return windowKeyCode;
 		}
 		return 0;
 	}
-	
+
 }
