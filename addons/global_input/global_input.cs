af48225d5bc360b56f9dad09b965a2b6d15bbd56
darnoman <darnomang@gmail.com>
2024-05-24 11:46:46 -0500

    changed to sharphook

--- addons/global_input/global_input.cs
+++ addons/global_input/global_input.cs
@@ -5,18 +5,18 @@ using System;
 [Tool]
 public partial class global_input : EditorPlugin
 {
-	private const string GlobalInputGdAutoloadName = "GlobalInput";
-	
+	private const string GlobalInputGDAutoloadName = "GlobalInput";
+
 	public override void _EnterTree()
 	{
 		// Initialization of the plugin goes here.
-		AddAutoloadSingleton(GlobalInputGdAutoloadName, "res://addons/global_input/autoloads/global_input_gdscript/GlobalInputGDScript.tscn");
+		AddAutoloadSingleton(GlobalInputGDAutoloadName, "res://addons/global_input/autoload/global_input_gdscript/GlobalInputGDScript.tscn");
 	}
 
 	public override void _ExitTree()
 	{
 		// Clean-up of the plugin goes here.
-		RemoveAutoloadSingleton(GlobalInputGdAutoloadName);
+		RemoveAutoloadSingleton(GlobalInputGDAutoloadName);
 	}
 }
 #endif
